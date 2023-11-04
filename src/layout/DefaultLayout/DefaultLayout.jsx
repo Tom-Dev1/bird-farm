a9b3cefad52cb9517f6380c7c8ac69b699415a20
Tong Truong Thanh <69966374+Tom-Dev1@users.noreply.github.com>
2023-11-05 02:55:39 +0700

    fix

--- src/layout/DefaultLayout/DefaultLayout.jsx
+++ src/layout/DefaultLayout/DefaultLayout.jsx
@@ -1,18 +1,22 @@
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 
 import PropTypes from 'prop-types';
 import styles from './DefaultLayout.module.scss';
 import classNames from 'classnames/bind';
+import Footer from '../components/Footer';
 
 const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
     return (
         <div className={cx('wrapper')}>
             <Header />
-
-            <div className={cx('container')}>{children}</div>
-
+            {/* <main> */}
+            <div className={cx('container')}>
+                {/* <Navbar /> */}
+                {children}
+            </div>
+            {/* </main> */}
             <Footer />
         </div>
     );
