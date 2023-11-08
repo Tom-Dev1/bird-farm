1f0f1fdbfa5089918eada162a141afb6c0c13502
Tong Truong Thanh <69966374+Tom-Dev1@users.noreply.github.com>
2023-11-08 21:52:42 +0700

    login

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { publicRoutes } from './routes';
 import UserLayout from './layout/UserLayout';
 import './App.css';
 function App() {
     return (
-        <BrowserRouter>
-            <div className="App">
-                <Routes>
-                    {publicRoutes.map((route, index) => {
-                        const Page = route.Component;
-                        const Layout = UserLayout;
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page />
-                                    </Layout>
-                                }
-                            />
-                        );
-                    })}
-                </Routes>
-            </div>
-        </BrowserRouter>
+
+        <div className="App">
+            <Routes>
+                {publicRoutes.map((route, index) => {
+                    const Page = route.Component;
+                    const Layout = UserLayout;
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
+                })}
+            </Routes>
+        </div>
+
     );
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,14 @@
 import Home from '../pages/Home/Home';
+import Login from '../pages/Login';
 import Products from '../pages/Products/Products';
 export const publicRoutes = [
     { path: '/', Component: Home },
     // { path: '/about', Component: Home },
     { path: '/products', Component: Products },
+
+    { path: '/login', Component: Login }
 ];
 
-export const privateRoutes = [];
+export const privateRoutes = [
+
+];
